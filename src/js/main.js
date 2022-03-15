8da0320ff8cac5fa761cfa8959f4f2ee18fa5a89
Dmitry KAzarov~ <d.kazarov1361@gmail.com>
2022-03-15 20:02:06 +0200

    add +1 test image and create base functional

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
-'use strict'
+'use strict';
+
+const slides = document.querySelectorAll('.slider__img-inner');
+const btnPrev = document.querySelector('.btnPrev');
+const btnNext = document.querySelector('.btnNext');
+
+let slideIndex = 0;
+
+const showSlides = () => {
+  slides.forEach(elem => (elem.style.display = 'none'));
+
+  slides[slideIndex].style.display = 'block';
+};
+
+btnPrev.addEventListener('click', () => {
+  if (slideIndex === 0) {
+    slideIndex = 0;
+  }
+  if (slideIndex > 0) {
+    showSlides((slideIndex -= 1));
+  }
+});
+
+btnNext.addEventListener('click', () => {});
+
+showSlides(slideIndex);
